8f81e76dc631e4e725c0f2693649993b305564a9
daniel <daniel.zusev@gmail.com>
2021-02-12 14:04:17 +0200

    test

--- src/main/java/com/project/trip4u/utils/Consts.java
+++ src/main/java/com/project/trip4u/utils/Consts.java
@@ -3,5 +3,5 @@ package com.project.trip4u.utils;
 public class Consts {
 
 	public static String BASE_URL = "https://www.triposo.com/api/20201111/poi.json?";
-	
+	public static String TRIPOSO_ACCOUNT = "";
 }
--- src/main/java/com/project/trip4u/utils/Credentials.java
+++ src/main/java/com/project/trip4u/utils/Credentials.java
@@ -2,7 +2,7 @@ package com.project.trip4u.utils;
 
 public class Credentials {
 	
-	public static String TRIPOSO_ACCOUNT = "";
+	public static String TRIPOSO_ACCOUNT = "dd";
 	public static String TRIPOSO_TOKEN = "";
 
 }
